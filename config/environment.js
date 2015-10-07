0c315b7f144145226fbcb95e3014dad908c14649
Daryl Seaver <daryl.seaver@gmail.com>
2015-10-07 15:02:54 -0700

    Add Emberfire dependencies and update environment.js with correct Firebase address

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'peoples-trimet',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://peoples-trimet.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
