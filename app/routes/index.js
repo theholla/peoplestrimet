b2ce630b30c7c479b206fc95cda773ea68131213
Guest User <student@epicodus.com>
2015-10-08 12:17:13 -0700

    form collects user input

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    saveAlert(params) {
+      var newAlert = this.store.createRecord('alert',params);
+      newAlert.save();
+      this.transitionTo('index');
+    }
+  }
 });
