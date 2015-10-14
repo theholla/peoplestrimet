59f492df44364b4d5cef75940970bbda26a4eb5d
Daryl Seaver <daryl.seaver@gmail.com>
2015-10-14 14:46:07 -0700

    Add filtering to data from route-directions.json

--- app/routes/route-directions.js
+++ app/routes/route-directions.js
@@ -2,8 +2,28 @@ import Ember from 'ember';
 import Routedirectionadapter from 'peoples-trimet/adapters/routedirections';
 
 export default Ember.Route.extend({
-  model: function() {
+  // model: function() {
+  //   var adapter = Routedirectionadapter.create();
+  //   var allroutes = adapter.find();
+  //   var northroutes = allroutes.filter(function(route){
+  //     if(route.direction_id === '0') {
+  //       return true;
+  //     } else {
+  //       return false;
+  //     }
+  //   });
+  //   return northroutes;
+  // }
+
+  model() {
     var adapter = Routedirectionadapter.create();
-    return adapter.find();
+    return Ember.RSVP.hash({
+      north: adapter.find().then(function(routes) {
+        return routes.filterBy('direction_id','0');
+      }),
+      south: adapter.find().then(function(routes) {
+        return routes.filterBy('direction_id','1');
+      })
+    });
   }
 });
