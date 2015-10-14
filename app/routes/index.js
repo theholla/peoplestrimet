a2d6070ce3d7f8aa2348bc043742b47e50b4988c
nifferdil <nifferdil@gmail.com>
2015-10-14 14:56:56 -0700

    alerts delete after 2 hours

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,16 +1,24 @@
 import Ember from 'ember';
+import delay from 'ember-delay/delay';
 
 export default Ember.Route.extend({
 
   model: function() {
-      return this.store.find('alert', {
-          orderBy: 'line'
-      });
+    return this.store.find('alert', {
+      orderBy: 'line'
+    });
   },
+
+  alertDelay: function(alert) {
+    return delay(7200000).then(function() {
+        alert.destroyRecord();
+  });
+},
 
   actions: {
     saveAlert(params) {
       var newAlert = this.store.createRecord('alert',params);
+      this.alertDelay(newAlert);
       newAlert.save();
     }
   }
