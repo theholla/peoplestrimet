d1304039bc2d1594157dc08352ff885609857d53
Daryl Seaver <daryl.seaver@gmail.com>
2015-10-15 11:00:48 -0700

    Add buttons to display alerts for each line.  Displays in single component with hide button to hide alerts.

--- app/components/line-display.js
+++ app/components/line-display.js
@@ -2,9 +2,33 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   viewLine: false,
+  activeLine: null,
   actions: {
     viewAlerts() {
       this.set('viewLine',true);
+    },
+    hideAlerts() {
+      this.set('viewLine',false);
+    },
+    viewYellow() {
+      this.set('viewLine',true);
+      this.set('activeLine',this.yellowalerts);
+    },
+    viewGreen() {
+      this.set('viewLine',true);
+      this.set('activeLine',this.greenalerts);
+    },
+    viewBlue() {
+      this.set('viewLine',true);
+      this.set('activeLine',this.bluealerts);
+    },
+    viewRed() {
+      this.set('viewLine',true);
+      this.set('activeLine',this.redalerts);
+    },
+    viewOrange() {
+      this.set('viewLine',true);
+      this.set('activeLine',this.orangealerts);
     }
   }
 
